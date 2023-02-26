2dc792f932b7c9498dfda95c438f1165e609d11e
Tripurendra Kowshik Yedida <ytkowshik@gmail.com>
2023-02-26 17:20:02 +0530

    Adding region

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+//#region  "Styles"
 import '#src/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//#endregion
+//#region "React Elements"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//#endregion
+//#region "Components"
 import Header from '#components/header-component';
 import MainComponent from '#components/main-component';
 import FooterComponent from '#components/footer-component';
 import Home from '#components/home-component';
 import About from '#components/about-component';
 import Contact from '#components/contact-component';
-
+//#endregion
 function App() {
-
   return (
     <Router>
       <Header />
