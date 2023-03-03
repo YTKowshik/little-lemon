63baf445a6b104e1a3d3193fc4a3fce862468b55
Tripurendra Kowshik Yedida <ytkowshik@gmail.com>
2023-03-03 19:52:06 +0530

    Form correction

--- src/components/forms/booking-form.js
+++ src/components/forms/booking-form.js
@@ -1,33 +1,33 @@
+import { Form, Button, InputGroup } from "react-bootstrap";
+
 const BookingForm = () => {
-    const arrMinutes = [];
-    const arrHours=[];
-    for(let i=10;i<22;i++){
-        arrHours.push(<option key={"opt_"+i}>{`${i}`.padStart(2,"0")}</option>);
-    }
-    for(var j=0;j<60;j+=15){
-        arrMinutes.push(<option key={"opt_"+j}>{`${j}`.padStart(2,"0")}</option>);
-    }
     return (
-        <form >
-            <label htmlFor="res-date">Choose date</label>
-            <input type="date" id="res-date" />
-            <label htmlFor="res-time">Choose time</label>
-            <select id="res-time ">
-                {arrHours}
-            </select>
-            <label htmlFor="res-min">Choose time</label>
-            <select id="res-min">
-                {arrMinutes}
-            </select>
-            <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" />
-            <label htmlFor="occasion">Occasion</label>
-            <select id="occasion">
-                <option>Birthday</option>
-                <option>Anniversary</option>
-            </select>
-            <input type="submit" value="Make Your reservation" />
-        </form>
+        <Form id="booking-form" >
+            <Form.Group>
+                <Form.Label htmlFor="res-date">Choose date</Form.Label>
+                <Form.Control type="date" id="res-date" min={new Date().toISOString().slice(0, 10)} />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label htmlFor="res-time">Choose time</Form.Label>
+                <InputGroup>
+                <Form.Control type="time"></Form.Control>
+                </InputGroup>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label htmlFor="guests">Number of guests</Form.Label>
+                <Form.Control type="number" placeholder="1" min="1" max="10" id="guests" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label htmlFor="occasion">Occasion</Form.Label>
+                <Form.Select id="occasion">
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                </Form.Select>
+            </Form.Group>
+            <Button className="mt-3" id="btn-Reserve" variant="primary" type="submit">
+                Make Your reservation
+            </Button>
+        </Form>
     );
 };
 
