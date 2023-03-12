11493c70d1c0f76d00ce7e78b0ef73a1968b7152
Tripurendra Kowshik Yedida <ytkowshik@gmail.com>
2023-03-12 13:50:12 +0530

    Validation

--- src/components/forms/booking-form.js
+++ src/components/forms/booking-form.js
@@ -1,35 +1,34 @@
+import { useRef, useState } from "react";
 import { Form, Button, InputGroup } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useFormik } from "formik";
-import * as Yup from 'yup';
+
 const BookingForm = ({ availableTimes, submitForm, setBookingData }) => {
     const navigate = useNavigate();
+    const [validated, setValidated] = useState(false);
     const dateVal = new Date();
-    const formik = useFormik({
-        initialValues: {
-            date: '',
-            time: '',
-            noOfGuests: 1,
-            occasion: '',
-        },
-        onSubmit: (values) => {
+    const [validForm, setValidForm] = useState(false);
+    const formElem = useRef();
+    const handleChange = (e) => {
+        if(formElem.current.checkValidity() === false){setValidForm(false);}
+        if(formElem.current.checkValidity() === true){setValidForm(true);}
+    }
+    const handleSubmit = (e) => {
+        if (e.currentTarget.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        if (e.currentTarget.checkValidity() === true) {
             setBookingData();
             return submitForm() ? navigate('/confirmed-booking') : false;
-        },
-        validationSchema: Yup.object({
-            date: Yup.string(),
-            time: Yup.string(),
-            noOfGuests: Yup.number().max(10,"Select number of seats to be booked"),
-            occasion: Yup.string(),
-        }).required(),
-    });
+        }
+        setValidated(true);
+    }
     return (
-        <Form id="booking-form" onSubmit={(e) => { e.preventDefault(); formik.handleSubmit() }} >
+        <Form noValidate validated={validated} ref={formElem} id="booking-form" onSubmit={handleSubmit} >
             <Form.Group>
                 <Form.Label htmlFor="res-date">Choose date</Form.Label>
-                <Form.Control type="date" id="res-date" min={new Date(dateVal.getFullYear(), dateVal.getMonth(), dateVal.getDate() + 1).toISOString().slice(0, 10)}
-                    value={formik.values.date} />
-                <Form.Label>{formik.errors.date ? formik.errors.date : null}</Form.Label>
+                <Form.Control required placeholder="" type="date" id="res-date" onChange={handleChange} min={new Date(dateVal.getFullYear(), dateVal.getMonth(), dateVal.getDate() + 1).toISOString().slice(0, 10)} />
+                <Form.Control.Feedback type="invalid">Please select a date for booking</Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
                 <Form.Label htmlFor="res-time">Choose time</Form.Label>
@@ -38,15 +37,12 @@ const BookingForm = ({ availableTimes, submitForm, setBookingData }) => {
                         {availableTimes?.times?.map(x => { return <option key={`time_${x}`}>{x}</option> })}
                     </Form.Select>
                 </InputGroup>
-                <Form.Label>{formik.errors.time ? formik.errors.time : null}</Form.Label>
+                <Form.Control.Feedback type="invalid">{ }</Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
                 <Form.Label htmlFor="guests">Number of guests</Form.Label>
-                <Form.Control type="number" placeholder="1" id="guests"
-                    onBlur={formik.handleBlur}
-                    onChange={formik.handleChange}
-                    value={formik.values.noOfGuests} />
-                <Form.Label>{formik.errors.noOfGuests ? formik.errors.noOfGuests : null}</Form.Label>
+                <Form.Control type="number" defaultValue={0} min="0" id="guests" onChange={handleChange} required />
+                <Form.Control.Feedback type="invalid">Enter number of guests</Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
                 <Form.Label htmlFor="occasion">Occasion</Form.Label>
@@ -54,10 +50,10 @@ const BookingForm = ({ availableTimes, submitForm, setBookingData }) => {
                     <option>Birthday</option>
                     <option>Anniversary</option>
                 </Form.Select>
-                <Form.Label>{formik.errors.occasion ? formik.errors.occasion : null}</Form.Label>
+                <Form.Control.Feedback type="invalid">{ }</Form.Control.Feedback>
             </Form.Group>
             <div className="m-0 p-0 d-flex align-center justify-center submit-div">
-                <Button className="mt-3" variant="primary" type="submit" disabled={formik.errors}>
+                <Button className="mt-3" variant="primary" type="submit" disabled={!validForm}>
                     Make Your reservation
                 </Button>
             </div>
